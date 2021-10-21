144f9cbca135283e1e623b85dffe032ae964e936
KamrunNKanta <kantakamrunn@gmail.com>
2021-10-20 22:57:03 -0400

    1st commit

---
+++ src/LearningConstructor.java
@@ -0,0 +1,17 @@
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class LearningConstructor {
+	//Global variable
+	public String MyName="Inaya";
+	
+	//this one is a default constructor
+	public LearningConstructor() {}
+	//paramitarized constructor
+	
+public LearningConstructor(String MyName ) {}
+	public static void main(String[] args) {
+	
+
+	}
+
+}
